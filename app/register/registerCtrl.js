b536d1ba23ced06528e085c20f8c8edaddc2d483
kiran paturi <kpaturi@smarttuition.com>
2016-05-12 08:36:12 +0530

    registration functionality

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -1,14 +1,55 @@
-
-function registerCtrl($scope){
-   $scope.firstName="Kiran";
-   var lastName="PVS"; 
-    
-    $scope.registerUser=function(){
-        console.log($scope);
+var registerCtrlFn = function ($scope) {
+    //create objects and work with the objects
+    $scope.register = {
+        firstName: "",
+        lastName: "",
+        age: "",
+        gender: "",
+        address: {
+            streetName: "",
+            zipcode: "",
+            country: "",
+            state: ""
+        }
     };
-}
 
+    $scope.countries = [
+        {
+            code: "IN",
+            name: "India",
+            phonecode: "+91"
+        },
+        {
+            code: "US",
+            name: "United State",
+            phonecode: "+1"
+        },
+        {
+            code: "CA",
+            name: "Canada",
+            phonecode: "+2"
+                       }
+    ];
+    $scope.statesList = [{
+            countryCode: "IN",
+            stateCode: "TN",
+            stateName: "Telangana"
+        },
+        {
+            countryCode: "IN",
+            stateCode: "AP",
+            stateName: "Andhra Pradesh"
+        }];
+
+
+    $scope.registerUser = function () {
+        $scope.register.address.country = $scope.selectedCountry.code;
+    };
+
+};
 angular.module('twitterApp.register')
-    .controller('registerCtrl',registerCtrl)
+    .controller('registerCtrl', ['$scope', registerCtrlFn]);
 
-
+//best practices involved
+//
+//array notation.
